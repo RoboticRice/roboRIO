4373489a198dbffa1d58608fbf465a35d5aef662
Solidworks <pantherbotics@gmail.com>
2016-05-31 16:58:14 -0700

    Intital Test

---
+++ src/main/java/com/github/roboticrice/Main.java
@@ -0,0 +1,20 @@
+package com.github.roboticrice;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Created by Joshua Freedman on 5/31/2016.
+ * Project: roboRIO-Template
+ */
+public class Main extends IterativeRobot{
+
+    @Override
+    public void disabledInit() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+}
